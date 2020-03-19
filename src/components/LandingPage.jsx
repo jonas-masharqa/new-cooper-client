d839617ff87bcb4002c7e2c8be61b31d672d0bd4
jonas <jonas.masharqa@gmail.com>
2020-03-19 15:44:45 +0100

    Adds handler for input fields, hooks for gender, distance, age.

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,17 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react';
+import DisplayCooperResult from './DisplayCooperResult';
 
 const LandingPage = () => {
+  const [gender, setGender] = useState('female');
+  const [values, setValues] = useState({
+    distance: '',
+    age: ''
+  });
+
+  const onChangeHandler = e => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+
+    setGender({ [name]: value });
+  };
+
   return (
     <>
-      <input name='distance' id='distance' placeholder='Distance' />
-      <select name='gender' id='gender'>
+      <input
+        name='distance'
+        id='distance'
+        placeholder='Distance'
+        onChange={onChangeHandler}
+      />
+
+      <select name='gender' id='gender' onChange={onChangeHandler}>
         <option value='female'>Female</option>
         <option value='male'>Male</option>
       </select>
-      
-      <input name='age' id='age' placeholder='Age' />
-    </>
-  )
-}
 
-export default LandingPage
+      <input name='age' id='age' placeholder='Age' onChange={onChangeHandler} />
+
+      <DisplayCooperResult 
+        distance={values.distance}
+        age={values.age}
+        gender={gender}
+      />
+    </>
+  );
+};
+
+export default LandingPage;
